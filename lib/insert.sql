10f638a7d729b8fb117427a0217690db758224ff
Henry Escobar <henrye@gmail.com>
2022-11-01 17:24:39 -0700

    commit tree 083a0fc3380df75c3bf893b2ed147b8bbbf18ac9

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 4, "M", "Brown", "Crazy", 1);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Rowdy", 5, "M", "Brown", "Crazy", 1);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Tabitha", 6, "F", "Black", "Normal", 1);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Sergeant Brown", 4, "M", "Brown", "Crazy", 0);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Melissa", 2, "F", "Black", "Normal", 1);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Grinch", 7, "M", "Black", "Crazy", 0);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    ("Wendy", 3, "F", "Brown", "Normal", 1);
+INSERT INTO bears
+    (name, age, sex, color, temperament, alive)
+VALUES
+    (NULL, 4, "F", "Black", "Normal", 1);
